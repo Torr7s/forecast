eb74cc9a614d5ad1d686e5fbfb1218e6bed8c194
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-09 19:19:49 -0300

    chore(tests/func): not able to make requests yet

--- tests/functional/forecast.test.ts
+++ tests/functional/forecast.test.ts
@@ -1,11 +1,9 @@
-import supertest from 'supertest';
-
 describe('Beach forecast functional tests', (): void => {
   it('should return a forecast with just a few times', async (): Promise<void> => {
-    /* app still to be built */
-    const { body, status } = await supertest(app).get('api/forecast');
+    /* Cannot read properties of undefined (reading 'get') */
+    const { body, status } = await global.testRequest.get('api/forecast');
 
-    expect(body).toBe({
+    expect(body).toEqual({
       status: 'ok',
       message: 'Forecast controller test'
     });
