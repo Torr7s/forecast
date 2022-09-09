7f1148860e77b7d414eac9701d540295b3087cca
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-09 17:09:19 -0300

    project: setup jest for e2e tests

---
+++ tests/jest.config.js
@@ -0,0 +1,17 @@
+import * as path from 'node:path';
+
+const root = path.resolve(__dirname, '..');
+const rootConfig = require(`${root}/jest.config.js`);
+
+module.exports = {
+  ...rootConfig, ... {
+    rootDir: root,
+    displayName: 'e2e-tests',
+    setupFilesAfterEnv: [
+      '<rootDir>/tests/jest.setup.ts'
+    ],
+    testMatch: [
+      '<rootDir>/tests/**/*.test.ts'
+    ]
+  }
+};
