039031e610aaadc9c420201deeb0e2c3e108742f
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-09 17:08:47 -0300

    project: setup jest for global tests

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+import * as path from 'node:path';
+
+const root = path.resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: [
+    '<rootDir>/src/**/*.test.ts'
+  ],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1',
+  }
+};
