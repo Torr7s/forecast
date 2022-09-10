b2222e68564a6a54a00d85b591774c92ddf5b6a2
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-10 19:29:27 -0300

    feat(src): create typings for stormglass

---
+++ src/typings/index.d.ts
@@ -0,0 +1,29 @@
+interface StormGlassPointSource {
+  [key: string]: number;
+}
+
+interface StormGlassPoint {
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+interface StormGlassForecastResponse {
+  hours: StormGlassPoint[];
+}
+
+interface NormalizedForecastPoint {
+  time: string;
+  waveHeight: number;
+  waveDirection: number;
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  windDirection: number;
+  windSpeed: number;
+}
