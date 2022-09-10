0bda2f5bad6fd410f14e94d039723536f5a4e0dd
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-09 21:30:47 -0300

    style(tests/func): remove error comment

--- tests/functional/forecast.test.ts
+++ tests/functional/forecast.test.ts
@@ -1,6 +1,5 @@
 describe('Beach forecast functional tests', (): void => {
   it('should return a forecast with just a few times', async (): Promise<void> => {
-    /* Cannot read properties of undefined (reading 'get') */
     const { body, status } = await global.testRequest.get('api/forecast');
 
     expect(body).toEqual({
