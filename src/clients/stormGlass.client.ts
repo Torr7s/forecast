550ee891e53356bed09ab2f683ba1111e7d39d29
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-09 23:34:29 -0300

    feat(clients/stormclass): create fetchPoints method

--- src/clients/stormGlass.client.ts
+++ src/clients/stormGlass.client.ts
@@ -1,1 +1,14 @@
-export class StormGlassClient {}
+import { AxiosResponse, AxiosStatic } from 'axios';
+
+export class StormGlassClient {
+  readonly stormGlassApiParams: string = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassApiSource: string = 'noaa';
+
+  constructor(protected request: AxiosStatic) {};
+
+  public async fetchPoints(lat: number, lng: number): Promise<AxiosResponse> {
+    const url: string = `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}&end=1592113802`;
+
+    return this.request.get(url)
+  }
+}
