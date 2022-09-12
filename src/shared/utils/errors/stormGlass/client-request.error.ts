affb0a0ef25487022cbebe40c0de29eaa646234d
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-11 23:33:58 -0300

    feat(utils/errors): create stormglass client request error class
    errors generated on the client side

---
+++ src/shared/utils/errors/stormGlass/client-request.error.ts
@@ -0,0 +1,9 @@
+import { InternalError } from '../internal-error';
+
+export class ClientRequestError extends InternalError {
+  constructor(message: string) {
+    const internalErrorMessage: string = `Unexpected error when trying to communicate to StormGlass: "${message}"`;
+
+    super(internalErrorMessage);
+  }
+}
