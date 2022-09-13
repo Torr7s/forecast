5596c0967d6f2ed7c42039f57453b00ddd84815e
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-13 19:33:37 -0300

    feat(errors/stormglass): accept error of type AxiosError in constructor
    - create error message formatting

--- src/shared/utils/errors/stormGlass/response.error.ts
+++ src/shared/utils/errors/stormGlass/response.error.ts
@@ -1,8 +1,15 @@
+import { AxiosError } from 'axios';
+
 import { InternalError } from '../internal-error';
 
 export class StormGlassResponseError extends InternalError {
-  constructor(message: string) {
-    const internalErrorMessage: string = `Unexpected error returned by the StormGlass service: ${message}`;
+  constructor(error: AxiosError) {
+    const data: any = error.response.data;
+    const status: number = error.response.status;
+
+    const stringified: string = JSON.stringify(data);
+
+    const internalErrorMessage: string = `Unexpected error returned by the StormGlass service: "Error: ${stringified} Code: ${status}"`;
 
     super(internalErrorMessage);
   }
