53125d82ffae31e5d1df8ebd34da39acfa4e6b58
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-14 15:55:38 -0300

    feat(database): make connectDb return Mongoose promise

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import config, { IConfig } from 'config';
 
 const dbConfig: IConfig = config.get('app.database');
 
-export const connectDb = async (): Promise<void> => {
-  await mongoose.connect(dbConfig.get('mongoUrl'));
-}
+export const connectDb = async (): Promise<Mongoose> => await mongoose.connect(dbConfig.get('mongoUrl'));
 
 export const closeDb = (): Promise<void> => mongoose.connection.close();
