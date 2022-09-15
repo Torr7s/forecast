223e74733ae39522cb05e6fe7706ae6bfffecdf9
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-15 19:06:29 -0300

    feat(controllers/forecast): wrap code with try catch block
    - return error with status 500

--- src/controllers/forecast.controller.ts
+++ src/controllers/forecast.controller.ts
@@ -4,6 +4,7 @@ import { Controller, Get } from '@overnightjs/core';
 import { ForecastService } from '@src/services/forecast.service';
 
 import { Beach, BeachModel } from '@src/shared/database/models/beach.model';
+
 import { TimeForecast } from '@src/typings';
 
 const forecastService = new ForecastService();
@@ -12,9 +13,17 @@ const forecastService = new ForecastService();
 export class ForecastController {
   @Get('')
   public async getForecastForLoggedUser(_: Request, response: Response): Promise<Response> {
-    const beaches: BeachModel[] = await Beach.find({});
-    const forecastData: TimeForecast[] = await forecastService.processForecastForBeaches(beaches);
+    try {
+      const beaches: BeachModel[] = await Beach.find({});
+      const forecastData: TimeForecast[] = await forecastService.processForecastForBeaches(beaches);
 
-    return response.status(200).send(forecastData);
+      return response.status(200).send(forecastData);
+    } catch (error) {
+      return response
+        .status(500)
+        .send({
+          error: 'Something went wrong'
+        });
+    }
   }
 }
