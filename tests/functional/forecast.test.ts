d23eab3a1a9abca0f4f2542bcb054211a7ade068
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-15 19:07:32 -0300

    tests(func/forecast): create test for error during processing

--- tests/functional/forecast.test.ts
+++ tests/functional/forecast.test.ts
@@ -43,4 +43,24 @@ describe('Beach forecast functional tests', (): void => {
     expect(status).toBe(200);
     expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
+
+  it('should return 500 if something goes wrong during processing', async (): Promise<void> => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authroization: (): boolean => true
+      }
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+      })
+      .replyWithError('Something went wrong');
+
+    const { status } = await global.testRequest.get('/api/forecast');
+
+    expect(status).toBe(500);
+  });
 });
