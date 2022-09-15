3d771ea260ebca8bd79c304fd909d99656cac480
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-15 18:51:37 -0300

    tests(func/forecast): create a beach before run a test
    - use nock for http request and mock its response
    - use apiForecastResponse1BeachFixture for its expectation

--- tests/functional/forecast.test.ts
+++ tests/functional/forecast.test.ts
@@ -1,69 +1,47 @@
+import nock from 'nock';
+
+import { Beach, BeachPosition } from '@src/shared/database/models/beach.model';
+
+import apiForecastResponse1BeachFixture from '@tests/fixtures/api_forecast_response_1_beach.json';
+import stormGlassWeather3HoursFixture from '@tests/fixtures/stormglass_weather_3_hours.json';
+
 describe('Beach forecast functional tests', (): void => {
+  beforeEach(async (): Promise<void> => {
+    await Beach.deleteMany({});
+
+    const defaultBeach = {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.E,
+    }
+
+    const beach = new Beach(defaultBeach);
+
+    await beach.save();
+  });
+
   it('should return a forecast with just a few times', async (): Promise<void> => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+        params: /(.*)/,
+        source: 'noaa',
+      })
+      .reply(200, stormGlassWeather3HoursFixture)
+
     const { body, status } = await global.testRequest.get('/api/forecast');
 
-    expect(body).toEqual([
-      {
-        time: '2022-09-10T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 141.289824,
-            name: 'Manly',
-            position: 'East',
-            rating: 1,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2022-09-10T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-            windSpeed: 100
-          }
-        ]
-      },
-      {
-        time: '2022-09-10T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 141.289824,
-            name: 'Manly',
-            position: 'East',
-            rating: 1,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2022-09-10T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100
-          }
-        ]
-      },
-      {
-        time: '2022-09-10T02:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 141.289824,
-            name: 'Manly',
-            position: 'East',
-            rating: 1,
-            swellDirection: 182.56,
-            swellHeight: 0.28,
-            swellPeriod: 3.44,
-            time: '2022-09-10T02:00:00+00:00',
-            waveDirection: 232.86,
-            waveHeight: 0.46,
-            windDirection: 321.5,
-            windSpeed: 100
-          }
-        ]
-      }
-    ]);
+    expect(body).toEqual(apiForecastResponse1BeachFixture);
+
     expect(status).toBe(200);
   });
 });
