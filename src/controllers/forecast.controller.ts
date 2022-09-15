de3644a8d03d85477e678e81a432ec231b3b4f52
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-15 18:49:20 -0300

    feat(src/controllers): return beaches processed forecast

--- src/controllers/forecast.controller.ts
+++ src/controllers/forecast.controller.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from 'express';
 import { Controller, Get } from '@overnightjs/core';
 
+import { ForecastService } from '@src/services/forecast.service';
+
+import { Beach, BeachModel } from '@src/shared/database/models/beach.model';
+import { TimeForecast } from '@src/typings';
+
+const forecastService = new ForecastService();
+
 @Controller('api/forecast')
 export class ForecastController {
-
   @Get('')
-  public getForecastForLoggedUser(_: Request, response: Response): void {
-    response.send({
-      status: 'ok',
-      message: 'Forecast controller test'
-    });
+  public async getForecastForLoggedUser(_: Request, response: Response): Promise<Response> {
+    const beaches: BeachModel[] = await Beach.find({});
+    const forecastData: TimeForecast[] = await forecastService.processForecastForBeaches(beaches);
+
+    return response.status(200).send(forecastData);
   }
 }
