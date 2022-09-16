f455807a007748cd8d11200a8e840c4492e37e17
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-16 11:36:35 -0300

    feat(controllers/users): extends BaseController class
    - wrap code with try catch block
    - pass the error to sendCreateUpdateErrorResponse method

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express';
 import { Controller, Post } from '@overnightjs/core';
 
+import { BaseController } from './base.controller';
+
 import { User, UserModel } from '@src/shared/database/models/user.model';
 
 @Controller('api/users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async create(request: Request, response: Response): Promise<Response> {
-    const user = new User(request.body);
-    const newUser: UserModel = await user.save();
-
-    return response.status(201).send(newUser);
+    try {
+      const user = new User(request.body);
+      const newUser: UserModel = await user.save();
+  
+      return response.status(201).send(newUser); 
+    } catch (error) {
+      return this.sendCreateUpdateErrorResponse(response, error);
+    }
   }
 }
