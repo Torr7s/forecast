37d685f421af7d5b330b132c5d56224f82c9d25c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-18 20:26:39 -0300

    feat(providers/auth): create signToken method

--- src/shared/container/providers/auth/auth.provider.ts
+++ src/shared/container/providers/auth/auth.provider.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export class AuthProvider {
   public static async hashPassword(password: string, salt: number = 9): Promise<string> {
@@ -7,5 +8,11 @@ export class AuthProvider {
 
   public static async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
     return bcrypt.compare(password, hashedPassword);
+  }
+
+  public static signToken(payload: object): string {
+    return jwt.sign(payload, process.env.MD5_HASH_KEY as string, {
+      expiresIn: '1d'
+    });
   }
 }
