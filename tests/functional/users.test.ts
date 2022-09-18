aaf6f399d313d8c285b13d18087519791d201a5c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-18 19:25:40 -0300

    tests(func/users): check for encrypted password
    - use jest matcher at merged object w/ password

--- tests/functional/users.test.ts
+++ tests/functional/users.test.ts
@@ -1,20 +1,35 @@
 import { User } from '@src/shared/database/models/user.model';
 
+import { AuthProvider } from '@src/shared/container/providers/auth/auth.provider';
+
 describe('Users functional tests', (): void => {
   beforeEach(async (): Promise<any> => await User.deleteMany({}));
 
   describe('When creating a new user', (): void => {
-    it('should create a user successfully', async (): Promise<void> => {
+    it('should create a new user with an encrypted password successfully', async (): Promise<void> => {
       const newUser = {
         name: 'John Doe',
         email: 'johndoe@gmail.com',
         password: 'youshallnotpass',
       }
 
-      const response = await global.testRequest.post('/api/users').send(newUser);
+      const { body, status } = await global.testRequest.post('/api/users').send(newUser);
 
-      expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      expect(status).toBe(201);
+
+      await expect(
+        AuthProvider.comparePasswords(
+          newUser.password,
+          body.password
+        )
+      ).resolves.toBeTruthy();
+
+      expect(body).toEqual(expect.objectContaining({
+        ...newUser,
+        ...{
+          password: expect.any(String)
+        }
+      }));
     });
 
     it('should return 422 when there is a validation error', async (): Promise<void> => {
