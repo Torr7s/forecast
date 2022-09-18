41fa514e91fcb171558756fdcc47e47dd64404ba
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-18 20:27:13 -0300

    feat(controllers/users): create user authentication route

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,6 +5,8 @@ import { BaseController } from './base.controller';
 
 import { User, UserModel } from '@src/shared/database/models/user.model';
 
+import { AuthProvider } from '@src/shared/container/providers/auth/auth.provider';
+
 @Controller('api/users')
 export class UsersController extends BaseController {
   @Post('')
@@ -12,10 +14,43 @@ export class UsersController extends BaseController {
     try {
       const user = new User(request.body);
       const newUser: UserModel = await user.save();
-  
-      return response.status(201).send(newUser); 
+
+      return response.status(201).send(newUser);
     } catch (error) {
       return this.sendCreateUpdateErrorResponse(response, error);
     }
+  }
+
+  @Post('auth')
+  public async authenticate(request: Request, response: Response): Promise<Response | void> {
+    const { email, password } = request.body;
+
+    const user: UserModel = await User.findOne({ email });
+
+    if (!user) {
+      return response
+        .status(401)
+        .send({
+          code: 401,
+          error: 'User not found!'
+        });
+    }
+
+    if (!(await AuthProvider.comparePasswords(password, user.password))) {
+      return response
+        .status(401)
+        .send({
+          code: 401,
+          error: 'Invalid password given!'
+        });
+    };
+
+    const token: string = AuthProvider.signToken(user.toJSON());
+
+    return response
+      .status(200)
+      .send({
+        token
+      })
   }
 }
