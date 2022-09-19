0824d43dbb08de85760087b41e112fe84aa5a2c1
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-19 15:14:09 -0300

    feat(models/beach): add reference to user model

--- src/shared/database/models/beach.model.ts
+++ src/shared/database/models/beach.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model } from 'mongoose';
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export enum BeachPosition {
   S = 'South',
@@ -13,6 +13,7 @@ export interface Beach {
   position: BeachPosition;
   lat: number;
   lng: number;
+  user: Schema.Types.ObjectId
 }
 
 const beachSchema = new mongoose.Schema<Beach>({
@@ -31,6 +32,11 @@ const beachSchema = new mongoose.Schema<Beach>({
   },
   lng: {
     type: Number,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   }
 }, {
