dbba7de24ca911c81a5793313be9c477e587db8d
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-19 15:16:17 -0300

    feat(providers/auth): change user token assignment methodology

--- src/shared/container/providers/auth/auth.provider.ts
+++ src/shared/container/providers/auth/auth.provider.ts
@@ -1,7 +1,11 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+import { UserModel } from '@src/shared/database/models/user.model';
+
 interface JwtPayload { sub: string; }
+
+const md5HashKey: string = process.env.MD5_HASH_KEY;
 
 export class AuthProvider {
   public static async hashPassword(password: string, salt: number = 9): Promise<string> {
@@ -12,13 +16,14 @@ export class AuthProvider {
     return bcrypt.compare(password, hashedPassword);
   }
 
-  public static signToken(payload: object): string {
-    return jwt.sign(payload, process.env.MD5_HASH_KEY as string, {
+  public static signToken(user: UserModel): string {
+    return jwt.sign({}, md5HashKey, {
+      subject: user.id,
       expiresIn: '1d'
     });
   }
 
   public static decodeToken(token: string): JwtPayload {
-    return jwt.verify(token, process.env.MD5_HASH_KEY as string) as JwtPayload;
+    return jwt.verify(token, md5HashKey) as JwtPayload;
   }
 }
