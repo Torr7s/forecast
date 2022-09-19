0debc9608aebbd5f657b16f459e33eb7d680bef2
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-19 15:17:48 -0300

    refactor(controllers/beaches): delete result variable

--- src/controllers/beaches.controller.ts
+++ src/controllers/beaches.controller.ts
@@ -1,19 +1,21 @@
 import mongoose from 'mongoose';
 
 import { Request, Response } from 'express';
-import { Controller, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
 
 import { Beach, BeachModel } from '@src/shared/database/models/beach.model';
 
+import { AuthMiddleware } from '@src/shared/infra/http/middlewares/auth.middleware';
+
 @Controller('api/beaches')
+@ClassMiddleware(AuthMiddleware)
 export class BeachesController {
   @Post('')
   public async create(request: Request, response: Response): Promise<Response> {
     try {
-      const beach = new Beach(request.body);
-      const result: BeachModel = await beach.save();
+      const beach: BeachModel = await new Beach(request.body).save();
 
-      return response.status(201).send(result);
+      return response.status(201).send(beach);
     } catch (error) {
       if (error instanceof mongoose.Error.ValidationError) {
         return response
