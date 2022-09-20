0b19a4ea3d3910a746ea9b8a46d3854d223deb58
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-20 19:54:19 -0300

    feat(infra/mongo): use node-config to get mongo connection url

--- src/shared/infra/mongo/index.ts
+++ src/shared/infra/mongo/index.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
+import config, { IConfig } from 'config';
+
+const mongoDbConfig: IConfig = config.get('app.database');
 
 export const connect = async (): Promise<void> => {
-  await mongoose.connect(process.env.MONGODB_URL as string);
+  await mongoose.connect(mongoDbConfig.get('mongoUrl'));
 }
 
 export const close = async (): Promise<void> => await mongoose.connection.close();
