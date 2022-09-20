0aba7be4b6ba1c3dc09502aa84d5ad527ab1ca55
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-20 19:51:52 -0300

    feat(src/logger): add logger options based on node-config setup

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,9 @@
 import pino from 'pino';
+import config from 'config';
 
 export default pino({
+  enabled: config.get('app.logger.enabled'),
+  level: config.get('app.logger.level'),
   transport: {
     target: 'pino-pretty',
     options: {
