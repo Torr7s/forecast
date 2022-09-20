3c7e5a9a7aac167f173210fe115cd8130f145856
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-20 13:50:52 -0300

    feat(controllers/forecast): setup AuthMiddleware
    - find beaches of a user

--- src/controllers/forecast.controller.ts
+++ src/controllers/forecast.controller.ts
@@ -1,20 +1,24 @@
 import { Request, Response } from 'express';
-import { Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
 
 import { ForecastService } from '@src/services/forecast.service';
 
 import { Beach, BeachModel } from '@src/shared/database/models/beach.model';
+
+import { AuthMiddleware } from '@src/shared/infra/http/middlewares/auth.middleware';
 
 import { TimeForecast } from '@src/typings';
 
 const forecastService = new ForecastService();
 
 @Controller('api/forecast')
+@ClassMiddleware(AuthMiddleware)
 export class ForecastController {
   @Get('')
-  public async getForecastForLoggedUser(_: Request, response: Response): Promise<Response> {
+  public async getForecastForLoggedUser(request: Request, response: Response): Promise<Response> {
     try {
-      const beaches: BeachModel[] = await Beach.find({});
+      const beaches: BeachModel[] = await Beach.find({ user: request.user });
+      
       const forecastData: TimeForecast[] = await forecastService.processForecastForBeaches(beaches);
 
       return response.status(200).send(forecastData);
