9e062fd5657f8a1b2d204add53543998ef7ff586
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-22 18:08:42 -0300

    feat(clients/stormglass): use cache for forecast points
    create specific methods for its handle

--- src/clients/stormGlass.client.ts
+++ src/clients/stormGlass.client.ts
@@ -5,6 +5,8 @@ import { Request, Response } from '@src/shared/utils/request';
 import { ClientRequestError } from '@src/shared/utils/errors/stormGlass/client-request.error';
 import { StormGlassResponseError } from '@src/shared/utils/errors/stormGlass/response.error';
 
+import CacheUtil from '@src/shared/utils/cache';
+
 import { 
   NormalizedForecastPoint, 
   StormGlassForecastResponse, 
@@ -20,13 +22,37 @@ export class StormGlassClient {
   readonly stormGlassApiParams: string = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassApiSource: string = 'noaa';
 
-  constructor(protected request: Request = new Request()) {};
+  constructor(
+    protected request: Request = new Request(),
+    protected cacheUtil: typeof CacheUtil = CacheUtil
+  ) {};
 
   public async fetchPointWeatherData(lat: number, lng: number): Promise<NormalizedForecastPoint[]> {
-    try {
-      const endTimestamp: number = DateProvider.getUnixTimeForAFutureDay(1);
+    const cachedForecastPoints: NormalizedForecastPoint[] = this.getForecastPointsFromCache(this.getCacheKey(lat, lng));
 
-      const url: string = `${stormGlassResourceConfig.get('apiUrl')}/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}&end=${endTimestamp}`;
+    if (cachedForecastPoints)
+      return cachedForecastPoints;
+
+    const forecastPoints: NormalizedForecastPoint[] = 
+      await this.getForecastPointsFromApi(
+        lat, 
+        lng
+      );
+
+    this.setForecastPointsInCache(this.getCacheKey(lat, lng), forecastPoints);
+
+    return forecastPoints;
+  }
+
+  protected async getForecastPointsFromApi(lat: number, lng: number): Promise<NormalizedForecastPoint[]> {
+    const endTimestamp: number = DateProvider.getUnixTimeForAFutureDay(1);
+
+    try {
+      const url: string = `${
+        stormGlassResourceConfig.get(
+          'apiUrl'
+        )
+      }/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}&end=${endTimestamp}`;
 
       const response: Response<StormGlassForecastResponse> = await this.request.get<StormGlassForecastResponse>(url, {
         headers: {
@@ -43,6 +69,26 @@ export class StormGlassClient {
 
       throw new ClientRequestError(error);
     }
+  }
+
+  private getForecastPointsFromCache(key: string): NormalizedForecastPoint[] | undefined {
+    const forecastPointsFromCache: NormalizedForecastPoint[] = this.cacheUtil.get<NormalizedForecastPoint[]>(key);
+
+    return forecastPointsFromCache ?? undefined;
+  }
+
+  private getCacheKey(lat: number, lng: number): string {
+    return `forecast_points_${lat}_${lng}`;
+  }
+
+  private setForecastPointsInCache(key: string, forecastPoints: NormalizedForecastPoint[]): boolean {
+    const cacheTtl: number = stormGlassResourceConfig.get('cacheTtl');
+
+    return this.cacheUtil.set(
+      key,
+      forecastPoints,
+      cacheTtl
+    )
   }
 
   private normalizeResponse(points: StormGlassForecastResponse): NormalizedForecastPoint[] {
