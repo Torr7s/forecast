e70df7671291a46fa5142b3a6b3869453a299752
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-23 00:26:51 -0300

    feat(repositories): create find method in BaseRepository
    - recreate BeachRepository as an interface with a method

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,9 +1,13 @@
 import { Beach } from '@src/shared/infra/mongo/models/beach.model';
 
 export type WithId<T> = { id: string } & T;
+export type FilterOptions = Record<string, unknown>;
 
 export interface BaseRepository<T> {
   create(data: T): Promise<WithId<T>>;
+  find(options: FilterOptions): Promise<WithId<T>[]>;
 }
 
-export type BeachRepository = BaseRepository<Beach>;
+export interface BeachRepository extends BaseRepository<Beach> {
+  findAllBeachesForUser(userId: string): Promise<WithId<Beach>[]>;
+}
