2b5fe6eb8440cc92cdfcdf099bbd31a3b2cb1c1c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-23 09:58:48 -0300

    feat(repositories): add findOne method to BaseRepo and create UserRepo interface

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,4 +1,5 @@
 import { Beach } from '@src/shared/infra/mongo/models/beach.model';
+import { User } from '@src/shared/infra/mongo/models/user.model';
 
 export type WithId<T> = { id: string } & T;
 export type FilterOptions = Record<string, unknown>;
@@ -6,8 +7,14 @@ export type FilterOptions = Record<string, unknown>;
 export interface BaseRepository<T> {
   create(data: T): Promise<WithId<T>>;
   find(options: FilterOptions): Promise<WithId<T>[]>;
+  findOne(options: FilterOptions): Promise<WithId<T> | undefined>;
 }
 
 export interface BeachRepository extends BaseRepository<Beach> {
   findAllBeachesForUser(userId: string): Promise<WithId<Beach>[]>;
+}
+
+export interface UserRepository extends BaseRepository<User> {
+  findByEmail(email: string): Promise<WithId<User> | undefined>;
+  findById(id: string): Promise<WithId<User> | undefined>;
 }
