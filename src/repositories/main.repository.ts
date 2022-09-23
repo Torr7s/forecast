a8ad38703b3085995f03564f32bf94f0f7143526
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-23 10:58:07 -0300

    style(repos/main): rename class

--- src/repositories/main.repository.ts
+++ src/repositories/main.repository.ts
@@ -12,7 +12,7 @@ export class DatabaseValidationError extends DatabaseError {};
 
 export class DatabaseUnknownClientError extends DatabaseError {};
 
-export abstract class Repository<T> implements BaseRepository<T> {
+export abstract class MainRepository<T> implements BaseRepository<T> {
   public abstract create(data: T): Promise<WithId<T>>;
   public abstract find(options: FilterOptions): Promise<WithId<T>[]>;
   public abstract findOne(options: FilterOptions): Promise<WithId<T> | undefined>;
