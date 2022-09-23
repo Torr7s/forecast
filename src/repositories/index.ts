06b6c1913de39d36999c2cd174c4027ba9324627
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-22 23:30:34 -0300

    feat(repositories): create type BeachRepository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,5 +1,9 @@
+import { Beach } from '@src/shared/infra/mongo/models/beach.model';
+
 export type WithId<T> = { id: string } & T;
 
 export interface BaseRepository<T> {
   create(data: T): Promise<WithId<T>>;
 }
+
+export type BeachRepository = BaseRepository<Beach>;
