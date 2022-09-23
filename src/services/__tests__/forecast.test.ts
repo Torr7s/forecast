a972d68970950ae746658c00b56cdce4800fb31d
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-23 11:27:16 -0300

    tests(services/forecast): add tests over forecast ordering

--- src/services/__tests__/forecast.test.ts
+++ src/services/__tests__/forecast.test.ts
@@ -3,7 +3,7 @@ import { StormGlassClient } from '@src/clients/stormGlass.client';
 import { ForecastService } from '../forecast.service';
 import { ForecastProcessingInternalError } from '@src/shared/utils/errors/forecast/processing.error';
 
-import { Beach, GeoPosition } from '@src/shared/infra/mongo/models/beach.model';
+import { ExistingBeach, GeoPosition } from '@src/shared/infra/mongo/models/beach.model';
 
 import { TimeForecast } from '@src/typings';
 
@@ -12,9 +12,28 @@ import stormGlassNormalized3HoursFixture from '@tests/fixtures/stormglass_normal
 jest.mock('@src/clients/stormGlass.client');
 
 describe('Forecast Service', (): void => {
+  const defaultBeaches: ExistingBeach[] = [
+    {
+      id: 'fake-beach-id1',
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: GeoPosition.E,
+      user: 'fake-user-id'
+    },
+    {
+      id: 'fake-beach-id2',
+      lat: -33.792726,
+      lng: 141.289824,
+      name: 'Dee Why',
+      position: GeoPosition.S,
+      user: 'fake-user-id'
+    },
+  ];
+
   const mockedStormGlassService = new StormGlassClient() as jest.Mocked<StormGlassClient>;
 
-  it('should return the forecast for mutiple beaches in the same hour with different ratings ordered by rating', async () => {
+  it('should return the forecast for mutiple beaches in the same hour with different ratings ordered by rating decreasing', async (): Promise<void> => {
     mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
       {
         swellDirection: 123.41,
@@ -44,23 +63,6 @@ describe('Forecast Service', (): void => {
         windSpeed: 100,
       }
     ]);
-
-    const beaches: Beach[] = [
-      {
-        lat: -33.792726,
-        lng: 151.289824,
-        name: 'Manly',
-        position: GeoPosition.E,
-        user: 'fake-user-id',
-      },
-      {
-        lat: -33.792726,
-        lng: 141.289824,
-        name: 'Dee Why',
-        position: GeoPosition.S,
-        user: 'fake-user-id',
-      }
-    ];
 
     const expectedResponse = [
       {
@@ -101,7 +103,236 @@ describe('Forecast Service', (): void => {
     ];
 
     const forecast: ForecastService = new ForecastService(mockedStormGlassService);
-    const beachesWithRating: TimeForecast[] = await forecast.processForecastForBeaches(beaches);
+    const beachesWithRating: TimeForecast[] = await forecast.processForecastForBeaches(
+      defaultBeaches,
+      'rating',
+      'desc'
+    );
+
+    expect(beachesWithRating).toEqual(expectedResponse);
+  });
+
+  it('should return the forecast for mutiple beaches in the same hour with different ratings ordered by rating increasing', async (): Promise<void> => {
+    mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
+      {
+        swellDirection: 123.41,
+        swellHeight: 0.21,
+        swellPeriod: 3.67,
+        time: '2022-09-10T00:00:00+00:00',
+        waveDirection: 232.12,
+        waveHeight: 0.46,
+        windDirection: 310.48,
+        windSpeed: 100,
+      }
+    ]);
+
+    mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
+      {
+        swellDirection: 64.26,
+        swellHeight: 0.15,
+        swellPeriod: 13.89,
+        time: '2022-09-10T00:00:00+00:00',
+        waveDirection: 231.38,
+        waveHeight: 2.07,
+        windDirection: 299.45,
+        windSpeed: 100,
+      }
+    ]);
+
+    const expectedResponse = [
+      {
+        time: '2022-09-10T00:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: GeoPosition.E,
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2022-09-10T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100
+          },
+          {
+            lat: -33.792726,
+            lng: 141.289824,
+            name: 'Dee Why',
+            position: GeoPosition.S,
+            rating: 3,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 13.89,
+            time: '2022-09-10T00:00:00+00:00',
+            waveDirection: 231.38,
+            waveHeight: 2.07,
+            windDirection: 299.45,
+            windSpeed: 100
+          }
+        ]
+      }
+    ];
+
+    const forecast: ForecastService = new ForecastService(mockedStormGlassService);
+    const beachesWithRating: TimeForecast[] = await forecast.processForecastForBeaches(
+      defaultBeaches,
+      'rating',
+      'asc'
+    );
+
+    expect(beachesWithRating).toEqual(expectedResponse);
+  });
+
+  it('should return the forecast for mutiple beaches in the same hour with different lng ordered by decreasing', async (): Promise<void> => {
+    mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
+      {
+        swellDirection: 123.41,
+        swellHeight: 0.21,
+        swellPeriod: 3.67,
+        time: '2022-09-10T00:00:00+00:00',
+        waveDirection: 232.12,
+        waveHeight: 0.46,
+        windDirection: 310.48,
+        windSpeed: 100,
+      }
+    ]);
+
+    mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
+      {
+        swellDirection: 64.26,
+        swellHeight: 0.15,
+        swellPeriod: 13.89,
+        time: '2022-09-10T00:00:00+00:00',
+        waveDirection: 231.38,
+        waveHeight: 2.07,
+        windDirection: 299.45,
+        windSpeed: 100,
+      }
+    ]);
+
+    const expectedResponse = [
+      {
+        time: '2022-09-10T00:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: GeoPosition.E,
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2022-09-10T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100
+          },
+          {
+            lat: -33.792726,
+            lng: 141.289824,
+            name: 'Dee Why',
+            position: GeoPosition.S,
+            rating: 3,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 13.89,
+            time: '2022-09-10T00:00:00+00:00',
+            waveDirection: 231.38,
+            waveHeight: 2.07,
+            windDirection: 299.45,
+            windSpeed: 100
+          }
+        ]
+      }
+    ];
+
+    const forecast: ForecastService = new ForecastService(mockedStormGlassService);
+    const beachesWithRating: TimeForecast[] = await forecast.processForecastForBeaches(
+      defaultBeaches,
+      'lng',
+      'desc'
+    );
+
+    expect(beachesWithRating).toEqual(expectedResponse);
+  });
+
+  it('should return the forecast for mutiple beaches in the same hour with different lng ordered by increasing', async (): Promise<void> => {
+    mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
+      {
+        swellDirection: 123.41,
+        swellHeight: 0.21,
+        swellPeriod: 3.67,
+        time: '2022-09-10T00:00:00+00:00',
+        waveDirection: 232.12,
+        waveHeight: 0.46,
+        windDirection: 310.48,
+        windSpeed: 100,
+      }
+    ]);
+
+    mockedStormGlassService.fetchPointWeatherData.mockResolvedValueOnce([
+      {
+        swellDirection: 64.26,
+        swellHeight: 0.15,
+        swellPeriod: 13.89,
+        time: '2022-09-10T00:00:00+00:00',
+        waveDirection: 231.38,
+        waveHeight: 2.07,
+        windDirection: 299.45,
+        windSpeed: 100,
+      }
+    ]);
+
+    const expectedResponse = [
+      {
+        time: '2022-09-10T00:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 141.289824,
+            name: 'Dee Why',
+            position: GeoPosition.S,
+            rating: 3,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 13.89,
+            time: '2022-09-10T00:00:00+00:00',
+            waveDirection: 231.38,
+            waveHeight: 2.07,
+            windDirection: 299.45,
+            windSpeed: 100
+          },
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: GeoPosition.E,
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2022-09-10T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100
+          }
+        ]
+      }
+    ];
+
+    const forecast: ForecastService = new ForecastService(mockedStormGlassService);
+    const beachesWithRating: TimeForecast[] = await forecast.processForecastForBeaches(
+      defaultBeaches,
+      'lng',
+      'asc'
+    );
 
     expect(beachesWithRating).toEqual(expectedResponse);
   });
@@ -109,8 +340,9 @@ describe('Forecast Service', (): void => {
   it('should return the forecast for a list of beaches', async (): Promise<void> => {
     mockedStormGlassService.fetchPointWeatherData.mockResolvedValue(stormGlassNormalized3HoursFixture);
 
-    const beaches: Beach[] = [
+    const beaches: ExistingBeach[] = [
       {
+        id: 'fake-beach-id',
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
@@ -196,12 +428,13 @@ describe('Forecast Service', (): void => {
   });
 
   it('should throwing internal processing error when something goes wrong during rating process', async (): Promise<void> => {
-    const beaches: Beach[] = [{
+    const beaches: ExistingBeach[] = [{
+      id: 'fake-beach-id',
       lat: -33.792726,
-      lng: 141.289824,
+      lng: 151.289824,
       name: 'Manly',
       position: GeoPosition.E,
-      user: 'fake-user-id'
+      user: 'fake-user-id',
     }];
 
     mockedStormGlassService.fetchPointWeatherData.mockRejectedValue('Error fetching data');
