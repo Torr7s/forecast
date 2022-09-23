4c73419618a8888d3727dca4b77ea81085ab0e26
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-23 00:30:49 -0300

    feat(repos/beach): implement BeachRepository and create findAll method

--- src/repositories/beach.repository.ts
+++ src/repositories/beach.repository.ts
@@ -1,11 +1,16 @@
 import { Model } from 'mongoose';
 
+import { BeachRepository, WithId } from '.';
 import { DefaultMongoRepository } from './mongo.repository';
 
 import { Beach } from '@src/shared/infra/mongo/models/beach.model';
 
-export class MongoBeachRepository extends DefaultMongoRepository<Beach> {
+export class MongoBeachRepository extends DefaultMongoRepository<Beach> implements BeachRepository {
   constructor(private beachModel: Model<Beach> = Beach) {
     super(beachModel);
+  }
+
+  public async findAllBeachesForUser(userId: string): Promise<WithId<Beach>[]> {
+    return this.find({ userId });
   }
 }
