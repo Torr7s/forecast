2e4939a13135c070dbf10d3d40a4e98eef236a9b
Matheus Torres <torresmiranda.rj@gmail.com>
2022-09-23 10:01:44 -0300

    feat(repos/user): create MongoUserRepository and implements its methods

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,20 @@
+import { Model } from 'mongoose';
+
+import { UserRepository, WithId } from '.';
+import { DefaultMongoRepository } from './mongo.repository';
+
+import { User } from '@src/shared/infra/mongo/models/user.model';
+
+export class MongoUserRepository extends DefaultMongoRepository<User> implements UserRepository {
+  constructor(private userModel: Model<User> = User) {
+    super(userModel);
+  }
+
+  public async findByEmail(email: string): Promise<WithId<User>> {
+    return this.findOne({ email });
+  }
+
+  public async findById(id: string): Promise<WithId<User>> {
+    return this.findOne({ _id: id });
+  }
+} 
